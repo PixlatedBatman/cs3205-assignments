117373c251f59a1d7da5fd5b57c202b1c6eacbfb
PixlatedBatman <ee23b030@smail.iitm.ac.in>
2026-02-07 21:18:07 +0530

    Initialized working pages

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+for i in 1; do
+  pandoc src/assignment$i.md \
+    --standalone \
+    --css ../static/style.css \
+    -o public/assignment$i.html
+done
+
+pandoc src/index.md \
+  --standalone \
+  --mathjax \
+  --css ../static/style.css \
+  --css ../static/override.css \
+  -o public/index.html
---
+++ src/index.md
@@ -0,0 +1,7 @@
+<div class="markdown-body">
+
+# Assignments
+
+- [Assignment 1](assignment1.html)
+
+</div>
